7f054f446ccf9d00c36183664aa3bd54d00988d6
Brando Diaz <brandodiazmont@gmail.com>
2024-06-16 13:52:52 -0400

    0.0.1

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-IG5DRY7D.mjs";import"./chunk-IIV5VXIW.mjs";import"./chunk-WGHSD5S6.mjs";import"./chunk-POBYDHU5.mjs";import"./chunk-OZKU5ZKH.mjs";import"./chunk-GRPJ7PLL.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
---
+++ server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{b as o,c as r,d as n}from"./chunk-IIV5VXIW.mjs";import{yb as e}from"./chunk-GRPJ7PLL.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
