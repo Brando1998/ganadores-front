c7c716fbae51420fc53014c565f40cb557ccc3ed
Brando Diaz <brandodiazmont@gmail.com>
2024-06-19 13:29:28 -0400

    fix

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-VMK3FUVO.mjs";import"./chunk-GH647DR2.mjs";import"./chunk-H3LGCYUR.mjs";import"./chunk-HRWFVXUO.mjs";import"./chunk-WRXCKRHJ.mjs";import"./chunk-F6KAAKFR.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
--- server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{b as o,c as r,d as n}from"./chunk-GH647DR2.mjs";import{zb as e}from"./chunk-F6KAAKFR.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
